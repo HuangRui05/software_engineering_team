bfed6f31dfb899f17a65d82b5251f4e14f85e7ee
HuangRui05 <2765885806@qq.com>
2026-04-23 18:20:32 +0800

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,1 +1,9 @@
+def test_login_right():
+    user = "admin"
+    pwd = "123456"
+    assert user == "admin"
+    assert pwd == "123456"
 
+def test_login_error():
+    wrong_pwd = "654321"
+    assert wrong_pwd != "123456"
